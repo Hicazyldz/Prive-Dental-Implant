916a7f0d6d229630a59745635bc06ea7b57518f6
Hicaz Yıldız <hicazyldz03@gmail.com>
2026-02-25 23:12:46 +0300

    Revert "Dev"

--- app/[locale]/PatientInfoSection/page.tsx
+++
@@ -1,9 +0,0 @@
-import PatientInfoSection from "@/components/sections/PatientInfoSection";
-import Navbar from "@/components/layout/Navbar";
-export default function ProductsPage() {
-  return (
-    <main>
-        <Navbar/>
-        <PatientInfoSection /></main>
-  );
-}
